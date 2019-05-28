38905f43062e37d8ccd6998f537e166a47fc50d5
Faulty X <40147859+faultyx@users.noreply.github.com>
2019-05-28 16:21:48 +0100

    Add files via upload

---
+++ src/commands/Command.js
@@ -0,0 +1,11 @@
+const Commando = require("discord.js-commando");
+
+module.exports = class Command extends Commando.Command {
+	constructor(client, info) {
+		super(client, info);
+		this.description = info.description || null;
+    this.examples = info.examples || [];
+		this.clientpermissions = info.clientpermissions || [];
+		this.userpermissions = info.userpermissions || [];
+	}
+};
---
+++ src/commands/utility/ping.js
@@ -0,0 +1,27 @@
+const Command = require("../Command");
+const Discord = require("discord.js");
+module.exports = class pingCommand extends Command {
+  constructor(client) {
+		super(client, {
+      name: "ping",
+      group: "utility",
+      memberName: "ping",
+      description: "See the message & api latency.",
+      examples: ["ping"],
+      aliases: []
+    });
+  }
+  async run(msg) {
+    const embed = new Discord.RichEmbed()
+    .setColor(null)
+    .setTitle("Ping?")
+    let message = await msg.channel.send(embed);
+    message.edit(new Discord.RichEmbed()
+    .setColor("BLUE")
+    .setTimestamp()
+    .setFooter(msg.author.tag, msg.author.displayAvatarURL)
+    .setTitle("🏓 Pong!")
+    .addField("Message:", `\`${message.createdTimestamp - msg.createdTimestamp}ms\``, true)
+    .addField("API:", `\`${Math.round(msg.client.ping)}ms\``, true))
+  };
+};
