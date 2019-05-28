973c3bd975d2fc9c7a7a7158dc9edf65b0e702a9
Faulty X <40147859+faultyx@users.noreply.github.com>
2019-05-28 16:22:16 +0100

    Update Command.js

--- src/commands/Command.js
+++ src/commands/Command.js
@@ -4,7 +4,7 @@ module.exports = class Command extends Commando.Command {
 	constructor(client, info) {
 		super(client, info);
 		this.description = info.description || null;
-    this.examples = info.examples || [];
+                this.examples = info.examples || [];
 		this.clientpermissions = info.clientpermissions || [];
 		this.userpermissions = info.userpermissions || [];
 	}
