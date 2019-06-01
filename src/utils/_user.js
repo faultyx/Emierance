f989b7b7f229f168b91f7cfe2410ab3f09076523
Faulty X <40147859+faultyx@users.noreply.github.com>
2019-06-01 16:04:15 +0100

    Update _user.js

--- src/utils/_user.js
+++ src/utils/_user.js
@@ -1,1 +1,12 @@
 const mongoose = require("mongoose");
+
+const User = new mongoose.Schema({
+  id: String,
+  name: String,
+  avatar: String,
+  created: Date,
+  description: String,
+  blacklisted: Boolean
+})
+
+module.exports = mongoose.model("User", User);
