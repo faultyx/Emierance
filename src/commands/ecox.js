0e67460ee72b91eca428cb873e0e89cd78bf6e46
Faulty X <40147859+faultyx@users.noreply.github.com>
2019-06-06 11:06:50 +0100

    this.savingAmount()

--- src/commands/ecox.js
+++ src/commands/ecox.js
@@ -1,1 +1,16 @@
-
+const _user = require("../utils/_user");
+const mongoose = require("mongoose");
+module.exports = ecox {
+  
+  this.savingAmount = new mongoose.Schema({
+    saved: String
+  });
+  
+  this.savingAmount.create({ size: "large", saved: 0 }).then(() => console.log("[savingAmount] Schema created.");
+                                                             
+  this.savingAmount.save();
+  
+                                                             
+ _user.updateOne({ avatar: null });
+  // to lower size, avatar will be cached again later.
+};
